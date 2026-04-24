3e4a7fc56de992c1c3deb271f458748d3d2f128b
Alya <4bitFox@tschudibacon.com>
2026-04-24 18:23:55 +0200

    nixos-rebuild switch

--- partitions.nix
+++ partitions.nix
@@ -7,17 +7,17 @@
 
 {
   boot = {
-    ### full disk encryption ###
+    ### full disk encryption; decrypt on boot ###
     initrd.luks.devices = {
       luksroot = {
-        # device = "/dev/disk/by-uuid/358b9e84-52cb-403c-b903-06ca33e3e520"; # adjust U>
+        # device = "/dev/disk/by-uuid/358b9e84-52cb-403c-b903-06ca33e3e520"; # adjust UUID after partitioning!
         device = "/dev/disk/by-label/GLaDOS_luks"; # Make sure label is correct!
         preLVM = true;
         allowDiscards = true;
       };
     };
 
-  ### swap ###
+  ### swap for hibernation ###
   resumeDevice = "/dev/dm-1"; # use "swapon -s" for path
   };
 }
