3f0d2fdeac2d99d4edafe6382e4572fbaca5d92f
Alya <4bitFox@tschudibacon.com>
2026-05-08 21:36:27 +0200

    nixos-rebuild switch

--- modules/nix.nix
+++ modules/nix.nix
@@ -13,11 +13,17 @@
       show-trace = true;
       substitute = true;
       fallback = true;
-      # max-jobs = "auto";
-      max-jobs = 4;
-      cores = 2;
+      max-jobs = "auto";
+      # max-jobs = 4;
+      cores = 0;
+      # cores = 2;
       keep-going = true;
       sandbox = true;
     };
+  };
+  
+  systemd.services.nix-daemon.serviceConfig = {
+    Nice = lib.mkForce 15;
+    IOSchedulingClass = lib.mkForce "idle";
   };
 }
