021e4d9506489d1c33e34f36df8c3e02e224a4d8
Alya <4bitFox@tschudibacon.com>
2026-05-31 21:06:56 +0200

    nixos-rebuild switch

--- modules/packages.nix
+++ modules/packages.nix
@@ -50,7 +50,7 @@
       libraries = with pkgs; [
         # Add any missing dynamic libraries for unpackaged programs
         # here, NOT in environment.systemPackages
-        libX11
+        libx11
       ];
     };
   };
