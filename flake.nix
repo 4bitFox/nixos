61afdc760982ba1690e6cf6280ef7d3fd60edd4c
Alya <4bitFox@tschudibacon.com>
2026-06-09 14:55:10 +0200

    nixos-rebuild switch

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
 
   inputs = {
     # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     ### home-manager ###
     # home-manager.url = github:nix-community/home-manager/ # unstable channel
-    home-manager.url = github:nix-community/home-manager/release-25.11; # stable channel
+    home-manager.url = github:nix-community/home-manager/release-26.05; # stable channel
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ### declarative-flatpak ###
     declarative-flatpak.url = "github:in-a-dil-emma/declarative-flatpak/latest";
