63456325c41b217495cff4fa4d8009683717bf96
Alya <4bitFox@tschudibacon.com>
2026-06-14 15:45:30 +0200

    nixos-rebuild switch

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,7 +2,7 @@
 { config, pkgs, lib, ... }:
 
 let
-bootlogo = ''
+  bootlogo = ''
                                               
                                               
                                               
