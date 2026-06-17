d1f223a5308c68a3e38b948f44756f8753a4d505
Alya <4bitFox@tschudibacon.com>
2026-06-17 07:55:06 +0200

    nixos-rebuild switch

--- hosts/tuxedo-sirius-16-gen2/specialisations/_gpu-passtrough.nix
+++ hosts/tuxedo-sirius-16-gen2/specialisations/_gpu-passtrough.nix
@@ -3,57 +3,6 @@
 
 
 {
-  imports = [
-    ./_partitions.nix
-    ./speicalisations/_gpu-passthrough.nix
-  ];
-
-  nix = {
-    settings = {
-      max-jobs = 4;
-      cores = 2;
-    };
-  };
-
-
-  hardware = {
-    tuxedo-drivers.enable = true;
-  };
-
-  systemd = {
-    sleep.settings.Sleep = {
-      SuspendState = "freeze";
-    };
-  };
-
-  services = {
-    ollama = {
-      enable = true;
-      package = pkgs.ollama-rocm; # Use AMD GPU for ollama
-      loadModels = [
-        # general
-        "llama3.1:8b"
-        # reasoning
-        "deepseek-r1:8b"
-      ];
-      syncModels = true;
-      rocmOverrideGfx = "11.0.2";
-    };
-  };
-
-  # boot.initrd.kernelModules = [ "amdgpu" ];
-
-  environment = {
-    systemPackages = with pkgs; [
-      radeontop
-      rocmPackages.rocminfo
-    ];
-    variables = {
-      RUSTICL_ENABLE = "radeonsi";
-    };
-  };
-
-  
   specialisation = {
     gpu-passtrough = {
       inheritParentConfig = true;
@@ -89,7 +38,7 @@
         };
 
         services.udev.packages = lib.singleton (pkgs.writeTextFile
-          { 
+          {
             name = "kvmfr";
             text = ''
               SUBSYSTEM=="kvmfr", GROUP="kvm", MODE="0660", TAG+="uaccess"
@@ -110,7 +59,7 @@
             ]
           '';
         };
-        
+
         environment = {
           systemPackages = with pkgs; [
             looking-glass-client
