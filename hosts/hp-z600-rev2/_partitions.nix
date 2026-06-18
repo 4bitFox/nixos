362e926b3c0c5d6d41a37ab1d3920e727d442283
Alya <4bitFox@tschudibacon.com>
2026-06-18 22:30:07 +0200

    nixos-rebuild switch

--- hosts/hp-z600-rev2/_partitions.nix
+++ hosts/hp-z600-rev2/_partitions.nix
@@ -7,4 +7,16 @@
     ### swap for hibernation ###
     resumeDevice = "/dev/dm-0"; # use "swapon -s" for path
   };
+
+  fileSystems."/mnt/fd" = {
+    device = "/dev/fd0";
+    fsType = "vfat";
+    options = [
+      "rw"
+      "users"
+      "nofail"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=1"
+    ];
+  };
 }
