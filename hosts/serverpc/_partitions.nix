a4895b450be00b4bba34494e2eda86c3111a2e72
Alya <4bitFox@tschudibacon.com>
2026-07-12 18:05:39 +0200

    nixos-rebuild switch

--- hosts/serverpc/_partitions.nix
+++ hosts/serverpc/_partitions.nix
@@ -8,8 +8,5 @@
     resumeDevice = "/dev/dm-0"; # use "swapon -s" for path
   };
 
-#  fileSystems."/data" = {
-#    device = "data";
-#    fsType = "zfs";
-#  };
+  boot.zfs.extraPools = [ "data" ];
 }
