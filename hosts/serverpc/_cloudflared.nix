d56e0c8dc3c8525e9b2da30fa271afc3ce6e68e2
Alya <4bitFox@tschudibacon.com>
2026-07-14 00:26:04 +0200

    nixos-rebuild switch

--- hosts/serverpc/_cloudflared.nix
+++ hosts/serverpc/_cloudflared.nix
@@ -14,7 +14,7 @@
       "3b9593b0-983e-458a-a35c-9ede085ef3a0" = {
         credentialsFile = "/data/cloudflared/3b9593b0-983e-458a-a35c-9ede085ef3a0.json";
         ingress = {
-          "nextcloud.tschudibacon.com" = "https://localhost:8080";
+          "nextcloud.tschudibacon.com" = "http://localhost:8080";
         };
 
         default = "http_status:404";
