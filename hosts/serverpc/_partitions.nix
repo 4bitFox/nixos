1489f1c01b270f1535b7a08af4c44c95e931c0af
Alya <4bitFox@tschudibacon.com>
2026-07-19 22:41:57 +0200

    nixos-rebuild switch

--- hosts/serverpc/_partitions.nix
+++ hosts/serverpc/_partitions.nix
@@ -9,4 +9,11 @@
   };
 
   boot.zfs.extraPools = [ "data" ];
+
+  fileSystems = {
+    "/backup" = {
+      device = "/dev/disk/by-uuid/205c59a3-7ff1-48f7-bcd7-fa7069e06fbf";
+      fsType = "ext4";
+    };
+  };
 }
