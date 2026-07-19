fcc899c19d8fe92653156e1233f88945a5a27147
Alya <4bitFox@tschudibacon.com>
2026-07-19 22:45:10 +0200

    nixos-rebuild switch

--- hosts/serverpc/_partitions.nix
+++ hosts/serverpc/_partitions.nix
@@ -14,6 +14,11 @@
     "/backup" = {
       device = "/dev/disk/by-uuid/205c59a3-7ff1-48f7-bcd7-fa7069e06fbf";
       fsType = "ext4";
+      options = [
+        "nofail"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=10s"
+      ];
     };
   };
 }
