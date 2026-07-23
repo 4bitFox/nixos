5262fc3b25e44b6a5e8db39f495eed4d0c08fef6
Alya <4bitFox@tschudibacon.com>
2026-07-23 19:45:17 +0200

    nixos-rebuild switch

--- hosts/serverpc/_klipper.nix
+++ hosts/serverpc/_klipper.nix
@@ -15,9 +15,19 @@
       port = 7125;
       settings = {
         # https://moonraker.readthedocs.io/en/latest/configuration/
+        authorization = {
+          trusted_clients = [
+            "127.0.0.1"
+            "192.168.1.0/24"
+          ];
+        };
       };
       allowSystemControl = false;
       stateDir = "/persist/var/lib/moonraker";
+    };
+
+    mainsail = {
+      enable = true;
     };
   };
 }
