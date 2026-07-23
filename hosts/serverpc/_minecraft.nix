99da89c679d08d0256597f88d3cd646b0e3590c5
Alya <4bitFox@tschudibacon.com>
2026-07-23 15:04:47 +0200

    nixos-rebuild switch

--- hosts/serverpc/_minecraft.nix
+++ hosts/serverpc/_minecraft.nix
@@ -34,7 +34,7 @@
         TimeoutStopSec = "180s";
         SuccessExitStatus = "130";
         ExecStart = ''
-          ${pkgs.screen}/bin/screen -DmS minecraft_b173 ${pkgs.jdk8}/bin/java -Xms2048M -Xmx16384M -jar poseidon-craftbukkit*.jar nogui
+          ${pkgs.screen}/bin/screen -DmS minecraft_b173 ${pkgs.jdk8}/bin/java -Xms2048M -Xmx16384M -jar poseidon-craftbukkit*.jar
 #          ${pkgs.bash}/bin/bash -c "exec ${pkgs.jdk8}/bin/java -Xms2048M -Xmx16384M -Djline.terminal=jline.UnsupportedTerminal -Dterminal.jline=false -Dterminal.ansi=false -jar poseidon-craftbukkit*.jar nogui"
         '';
         ExecStop = ''
