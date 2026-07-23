030e52d78b2e7370440ee1738551dbdf75701fe6
Alya <4bitFox@tschudibacon.com>
2026-07-23 21:12:21 +0200

    nixos-rebuild switch

--- hosts/serverpc/_klipper.nix
+++ hosts/serverpc/_klipper.nix
@@ -42,9 +42,14 @@
           allow 192.168.1.0/24;
           deny all;
         '';
-        locations."/moonraker/" = {
-          proxyPass = "http://127.0.0.1:7125/";
-          proxyWebsockets = true;
+        locations = {
+          "/moonraker/" = {
+            proxyPass = "http://127.0.0.1:7125/";
+            proxyWebsockets = true;
+          };
+          "/" = {
+            proxyPass = "http://127.0.0.1:7125/";
+          };
         };
       };
     };
