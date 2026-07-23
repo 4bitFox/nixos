d483923fed69711a1ae040ec72b322312b84a9cc
Alya <4bitFox@tschudibacon.com>
2026-07-23 19:19:25 +0200

    nixos-rebuild switch

--- hosts/serverpc/_klipper.nix
+++ hosts/serverpc/_klipper.nix
@@ -10,6 +10,7 @@
         # https://www.klipper3d.org/Overview.html#configuration-and-tuning-guides
       };
       configDir = "/persist/var/lib/klipper";
+      configFile = klipper/printer.cfg
     };
 
     moonraker = {
