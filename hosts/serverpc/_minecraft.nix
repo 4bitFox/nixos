48a57a207bf539ef3d0eb0372abd1d2fa454b5a0
Alya <4bitFox@tschudibacon.com>
2026-07-24 22:45:48 +0200

    nixos-rebuild switch

--- hosts/serverpc/_minecraft.nix
+++ hosts/serverpc/_minecraft.nix
@@ -14,7 +14,10 @@
         WorkingDirectory = "/data/minecraft";
         TimeoutStopSec = "180s";
         ExecStart = ''
-          ${pkgs.screen}/bin/screen -S minecraft ${pkgs.bash}/bin/bash -c "${pkgs.jdk21}/bin/java -Xms2048M -Xmx16384M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:InitiatingHeapOccupancyPercent=40 -XX:G1ReservePercent=10 -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -XX:G1HeapRegionSize=8M -jar ./paper-*.jar nogui"
+          ${pkgs.screen}/bin/screen -DmS minecraft ${pkgs.bash}/bin/bash -c "${pkgs.jdk21}/bin/java -Xms2048M -Xmx16384M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:InitiatingHeapOccupancyPercent=40 -XX:G1ReservePercent=10 -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=20 -XX:G1HeapRegionSize=8M -jar ./paper-*.jar nogui"
+          while ${pkgs.screen}/bin/screen -list | grep -q minecraft; do
+            sleep 1
+          done
         '';
         ExecStop = ''
           ${pkgs.screen}/bin/screen -S minecraft -p 0 -X stuff "stop\r" || true
@@ -36,7 +39,10 @@
         WorkingDirectory = "/data/minecraft_b173";
         TimeoutStopSec = "180s";
         ExecStart = ''
-          ${pkgs.screen}/bin/screen -S minecraft_b173 ${pkgs.bash}/bin/bash -c "${pkgs.jdk8}/bin/java -Xms2048M -Xmx16384M -jar poseidon-craftbukkit*.jar nogui"
+          ${pkgs.screen}/bin/screen -DmS minecraft_b173 ${pkgs.bash}/bin/bash -c "${pkgs.jdk8}/bin/java -Xms2048M -Xmx16384M -jar poseidon-craftbukkit*.jar nogui"
+          while ${pkgs.screen}/bin/screen -list | grep -q minecraft_b173; do
+            sleep 1
+          done
         '';
         ExecStop = ''
           ${pkgs.screen}/bin/screen -S minecraft_b173 -p 0 -X stuff "stop\r" || true
@@ -59,7 +65,10 @@
         WorkingDirectory = "/data/minecraft_b173/ViaProxy";
         TimeoutStopSec = "180s";
         ExecStart = ''
-          ${pkgs.screen}/bin/screen -S minecraft_b173_viaproxy ${pkgs.bash}/bin/bash -c "${pkgs.jdk21}/bin/java -jar ViaProxy*.jar config viaproxy.yml"
+          ${pkgs.screen}/bin/screen -DmS minecraft_b173_viaproxy ${pkgs.bash}/bin/bash -c "${pkgs.jdk21}/bin/java -jar ViaProxy*.jar config viaproxy.yml"
+          while ${pkgs.screen}/bin/screen -list | grep -q minecraft_b173_viaproxy; do
+            sleep 1
+          done
         '';
         ExecStop = ''
           ${pkgs.screen}/bin/screen -S minecraft_b173_viaproxy -p 0 -X stuff "stop\r" || true
