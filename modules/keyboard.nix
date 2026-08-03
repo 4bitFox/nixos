60979e002d8c7a9ff22fe801d15370fec0383645
Alya <4bitFox@tschudibacon.com>
2026-08-03 17:02:19 +0200

    nixos-rebuild switch

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -18,10 +18,10 @@
       type = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-mozc
-        fcitx5-chinese-addons
         fcitx5-gtk
-        fcitx5-configtool
         fcitx5-rose-pine
+        qt6Packages.fcitx5-chinese-addons
+        qt6Packages.fcitx5-configtool
       ];
     };
   };
