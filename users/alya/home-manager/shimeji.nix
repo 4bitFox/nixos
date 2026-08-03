e1e98cb55eb7d78f95b0fa7493a35d85647fec94
Alya <4bitFox@tschudibacon.com>
2026-08-04 01:06:12 +0200

    nixos-rebuild switch

--- users/alya/home-manager/shimeji.nix
+++ users/alya/home-manager/shimeji.nix
@@ -6,16 +6,6 @@
     packages = [
       wl_shimeji.packages.${pkgs.system}.default
     ];
-    #file = {
-      #".local/share/wl_shimeji" = {
-      #  source = ./homefiles/.local/share/wl_shimeji;
-      #  recursive = true;
-      #};
-      #".local/opt/wl_shimeji" = {
-      #  source = ./homefiles/.local/opt/wl_shimeji;
-      #  recursive = true;
-      #};
-    #};
     activation.wl_shimeji = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       rm -rf "$HOME/.local/share/wl_shimeji"
       mkdir -p "$HOME/.local/share/wl_shimeji"
