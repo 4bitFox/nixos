dadb2112252fe86e17fb0c03229c2d7b77142b78
Alya <4bitFox@tschudibacon.com>
2026-08-03 23:38:58 +0200

    nixos-rebuild switch

--- users/alya/home-manager/shimeji.nix
+++ users/alya/home-manager/shimeji.nix
@@ -8,7 +8,7 @@
         source = ./homefiles/.local/share/wl_shimeji;
         recursive = true;
       };
-      ".local/opt/" = {
+      ".local/opt/wl_shimeji" = {
         source = ./homefiles/.local/opt/wl_shimeji;
         recursive = true;
       };
